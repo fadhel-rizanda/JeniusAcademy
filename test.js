7fb96e75f202266ccc58bc5cbae3242d03e2b86a
fadhel-rizanda <fadhelbaihaqir25@gmail.com>
2024-07-13 19:33:58 +0700

    add deviding to code

--- test.js
+++ test.js
@@ -12,3 +12,5 @@ console.log("Substracting => " + substract(148, 74))
 const multiply = (a, b)=>a*b;
 console.log("Multiplying => " + multiply(37, 2));
 
+const divide = (a,b)=>a/b;
+console.log("dividing => " + divide(148, 2));
