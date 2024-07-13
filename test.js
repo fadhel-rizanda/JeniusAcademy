d97788fadff0fda264946a29badfde6b27890213
fadhel-rizanda <fadhelbaihaqir25@gmail.com>
2024-07-13 18:43:42 +0700

    update branch

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 console.log("Github Testing");
 console.log("Github Testing");
 
-const add = (a,b)=>a+b;
-console.log(add(37, 37));
+const add = (a, b)=>a+b;
+console.log("Adding => " + add(37, 37));
+
+const substract =  (a, b)=>a-b;
+console.log("Substracting => " + substract(148, 74))
